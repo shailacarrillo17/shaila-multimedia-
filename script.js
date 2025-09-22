6d745949f92c632b42b52c5e9265a12f9bc44107
shailacarrillo17 <shailacarrillo1705@gmail.com>
2025-09-22 22:54:12 +0000

    cambios lunes

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const play = document.getElementById('play');
+
 const audio1 = document.getElementById('audio1');
+const audio2 = document.getElementById('audio2');
+const audio3 = document.getElementById('audio3');
+
+const mañana = document.getElementById("mañana");
+const tarde = document.getElementById('tarde');
+const noche = document.getElementById('noche');
+
 
 let reproducir = false;
 
-document.body.addEventListener('click', () => {
+mañana.addEventListener('click', () => {
     if (reproducir===false) {
     audio1.play();
     }
@@ -13,4 +20,31 @@ document.body.addEventListener('click', () => {
     reproducir = !reproducir;
 })
 
+tarde.addEventListener('click', () => {
+    if (reproducir===false) {
+    audio2.play();
+    }
+    else {
+        audio2.pause();
+    }
+    reproducir = !reproducir;
+})
 
+noche.addEventListener('click', () => {
+    if (reproducir===false) {
+    audio3.play();
+    }
+    else {
+        audio3.pause();
+    }
+    reproducir = !reproducir;
+})
+
+const imagenes = [
+    "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiTSXfHq19jQJFg7sF16elOZYcDbJCk1fD9lq6oVKOJ3WBB5IcWRKhr7hGRplpAuS6fAqDaiZL4EYUgIHQnYS-hOrOEmWVSgN-WYXmW28FCVOKt-KkQi7mvtIPYF_9pxiRVGUOa6lTj5Qs/s1600/GranoleriaAguaPatos001a.jpg",
+];
+
+const imagen = document.getElementById("foto");
+const titulo = document.getElementById("titulo1");
+const prediccion = document.getElementById("prediccion1");
+const boton = document.getElementById("boton");
